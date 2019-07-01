4a32afe8b18ec6ab971b06803d9457c8350a5daa
WangaiJM <jahkeyjohn@gmail.com>
2019-07-01 04:52:36 +0300

    Compiling Assets and basic home and about page

---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,7 @@
+<div class="jumbotron text-center">
+    <div class="container">
+        <h1>welcome to our site</h1>
+        <p class="lead">welcome to our brand new laravel powered website.</p>
+        <p>This site uses laravel 5.8</p>
+    </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Basic Website</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
 
-    @include('inc.sidebar')
+    @include('inc.navbar')
 
+    <div class="container">
+        @if(Request::is('/'))
+            @include('inc.showcase')
+        @endif
+        <div class="row mt-3">
+            <div class="col-md-8 col-lg-8">
+                @yield('content')
+            </div>
+
+            <div class="col-md-4 col-lg-4">
+                @include('inc.sidebar')
+            </div>
+        </div>
+    </div>
+
+    <footer id="footer" class="text-center">
+        <p>Copyright 2019 &copy; Basic Website</p>
+    </footer>
 </body>
 </html>
